37930837167ee99f49787ebde1ff7ff2a3b276a0
Patrick Stapfer <ps@spp.at>
2015-06-25 14:09:21 +0200

    Fixes put.sh target path
    Adds id to the target path (/traces => /traces/1, etc.)

--- put.sh
+++ put.sh
@@ -2,9 +2,9 @@
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-#curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/0.json" http://localhost:9393/traces
-#curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/1.json" http://localhost:9393/traces
-#curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/2.json" http://localhost:9393/traces
-#curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/3.json" http://localhost:9393/traces
-#curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/4.json" http://localhost:9393/traces
-curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/5.json" http://localhost:9393/traces
+curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/0.json" http://localhost:9393/traces/0
+curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/1.json" http://localhost:9393/traces/1
+curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/2.json" http://localhost:9393/traces/2
+curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/3.json" http://localhost:9393/traces/3
+curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/4.json" http://localhost:9393/traces/4
+curl -i -H "Content-Type: application/json" -X PUT -d "@${SCRIPT_DIR}/fixture/5.json" http://localhost:9393/traces/5
